bbf976b1808af4c39fea332279f3aa8951cf521f
Fredrick Ritchie Eusebious Rajan <130365973+fredritchie@users.noreply.github.com>
2024-05-15 13:50:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.get('/', (req, res) => {
   </head>
   <body>
     <div class="container">
-      <h1>Welcome to Hypha DevOps Cohorts</h1>
+      <h1>Welcome to Hypha DevOps Solutions Architect Cohorts</h1>
       <h2>Enter your name here</h2>
       <form id="userForm">
         <label for="firstName">First Name:</label>
